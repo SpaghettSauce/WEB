59ddd2846db150fce4824a63ff0f94712b8b39c6
SpaghettiSauce <dontstarve221@gmail.com>
2024-03-09 11:03:41 +0200

    task1

---
+++ Lab2/code/index.php
@@ -0,0 +1,19 @@
+<?php
+//1
+/* Imagine a lot of code here*/
+
+$very_bad_unclear_name = "15 chicken wings";
+
+
+//Write your code here:
+
+$order = &$very_bad_unclear_name;
+$order .= ' and a large Dobriy cola';
+
+
+//Dont change the line below
+
+echo "\nYour order is:$very_bad_unclear_name.";
+
+//2
+
