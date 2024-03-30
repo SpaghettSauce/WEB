a6a74ed0fe395dac610fedcce102aeafeaa91a2e
Даня Ходосевич <114701605+SpaghettSauce@users.noreply.github.com>
2024-03-30 15:16:37 +0200

    added task 2a

--- Lab3/code/index.php
+++ Lab3/code/index.php
@@ -1,12 +1,12 @@
 <?php
-header('Content-type: text/plain');//из-за того что код в браузере html, "\n" воспринимается как пустое место, поэтому использую такой work around
+//header('Content-type: text/plain');
 
 //1.1
 
 $str = 'ahb acb aeb aeeb adcb axeb';
-echo("$str \n");
+echo("$str <br />");
 echo preg_replace('#a.b#','',$str);
-echo (" <--строки начинающиеся на а и заканчивающиеся на b с двумя символами между ними \n\n");
+echo ("  <--строки начинающиеся на а и заканчивающиеся на b с двумя символами между ними <br />");
 
 //1.2
 
@@ -16,3 +16,43 @@ $result = preg_replace_callback('/\d+/', function($matches) {
 }, $string);
 
 echo $result; 
+
+//2.1
+
+?>
+
+
+<!DOCTYPE html>
+<head>
+    <title>Task 2.1</title>
+</head>
+<body>
+    <form method = 'Post'>
+        <br>
+        <textarea name='inputText' rows='7' cols='70'></textarea>
+        <br><br>
+        <button type='submit' name='submit'>Посчитть</button>
+    </form>
+</body>
+
+<?php
+    if ('POST' === $_SERVER['REQUEST_METHOD']) {
+        if(isset($_POST['submit'])){
+            $text = $_POST['inputText'];
+            $wordCount = str_word_count($text);
+            $charCount = strlen($text);
+
+            echo "Кол-во слов =  $wordCount  <br />";
+            echo "Кол-во символов =  $charCount  <br />";
+        }
+    }
+    echo '<br />';
+?>
+
+<html lang="en">
+<body>
+    <form method='post'>
+        
+    <a href="Task2b.php">Task 2.</a>
+    </form>
+</body>
