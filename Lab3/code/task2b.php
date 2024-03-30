f29b8ab0ee33ddea4eb7c09c55e078170891fa06
Даня Ходосевич <114701605+SpaghettSauce@users.noreply.github.com>
2024-03-30 15:44:37 +0200

    added task 2b

--- Lab3/code/task2b.php
+++ Lab3/code/task2b.php
@@ -1,1 +1,20 @@
 <!DOCTYPE html>
+<html>
+<head>
+    <title>Task 2_2</title>
+</head>
+<body>
+    <form action="/Task2bSession.php" method="post">
+        <label for="surname">Фамилия</label>
+        <input type="text" name="surname" required >
+        <br><br>
+        <label for="name">Имя</label>
+        <input type="text" name="name" required>
+        <br><br>
+        <label for="age">Возраст</label>
+        <input type="text" name="age" required>
+        <br><br>
+        <input type="submit" value="Save">
+    </form>
+</body>
+</html>
