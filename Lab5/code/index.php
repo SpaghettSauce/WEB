4b8cd2125ce9198fcb11a61d638d3c96a766e904
Даня Ходосевич <114701605+SpaghettSauce@users.noreply.github.com>
2024-06-04 21:40:03 +0200

    added index.php

---
+++ Lab5/code/index.php
@@ -0,0 +1,26 @@
+<?php
+/* Подключение к серверу MySQL */
+$mysqli = new mysqli('db', 'root', 'helloworld', 'web');
+
+if (mysqli_connect_errno()) {
+    printf("Подключение к серверу MySQL невозможно. Код ошибки: %s\n", mysqli_connect_error());
+    exit;
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = $mysqli->real_escape_string($_POST['email']);
+    $title = $mysqli->real_escape_string($_POST['title']);
+    $category = $mysqli->real_escape_string($_POST['categories']);
+    $description = $mysqli->real_escape_string($_POST['text']);
+
+    $mysqli->query("INSERT INTO ad (email, title, description, category) VALUES ('$email', '$title', '$description', '$category')");
+}
+
+$ads = [];
+if ($result = $mysqli->query('SELECT * FROM ad ORDER BY created DESC')) {
+    while ($row = $result->fetch_assoc()) {
+        $ads[] = $row;
+    }
+    $result->close();
+}
+$mysqli->close();
